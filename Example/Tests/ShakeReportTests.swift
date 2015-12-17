fc079f2767ec9a5619cc3dc0bf81616c900d51e2
git <dtrenz@gmail.com>
2015-12-17 17:18:31 -0500

    Added tests for events triggered by shaking.

--- Example/Tests/ShakeReportTests.swift
+++ Example/Tests/ShakeReportTests.swift
@@ -1,23 +1,59 @@
 import UIKit
 import XCTest
 
+import Nimble
+
 @testable import ShakeReport
 
 class ShakeReportTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  override func setUp() {
+    super.setUp()
+  }
+    
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  // MARK: Shake Tests
+  
+  func testViewControllerCanBecomeFirstResponder() {
+    let viewController = UIViewController()
+    
+    expect(viewController.canBecomeFirstResponder()).to(beTrue())
+  }
+  
+  func testShakingTriggersScreenshot() {
+    class MockViewController: UIViewController {
+      var captureScreenshotWasCalled = false
+      
+      override func captureScreenshot() -> UIImage? {
+        captureScreenshotWasCalled = true
+        return nil
+      }
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    let viewController = MockViewController()
+    
+    viewController.motionEnded(.MotionShake, withEvent: nil)
+    
+    expect(viewController.captureScreenshotWasCalled).to(beTrue())
+  }
+  
+  func testShakingPresentsReportPrompt() {
+    class MockViewController: UIViewController {
+      var presentReportPromptWasCalled = false
+      
+      override func presentReportPrompt(reportActionHandler: (UIAlertAction) -> Void) {
+        presentReportPromptWasCalled = true
+      }
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
+    let viewController = MockViewController()
+    
+    viewController.motionEnded(.MotionShake, withEvent: nil)
+    
+    expect(viewController.presentReportPromptWasCalled).to(beTrue())
+  }
     
 }
